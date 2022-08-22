ba733533aea6fadf91b706b7892de78e48fbd830
badr elyatim <flipbadr7@gmail.com>
2022-08-22 20:26:25 +0100

    walter-donationV1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Donation;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +15,45 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Donation::create([
+            "firstname" => "John",
+            "lastname" =>  "Doe",
+            "email" => "sb-guuib20392647@personal.example.com",
+            "country_code" => "MA",
+            "amount" => 10,
+            "currency_code" => "USD"
+        ]);
+        Donation::create([
+            "firstname" => "omar",
+            "lastname" =>  "lmbkbk",
+            "email" => "sb-ohy94720349163@personal.example.com",
+            "country_code" => "US",
+            "amount" => 10,
+            "currency_code" => "USD"
+        ]);
+        Donation::create([
+            "firstname" => "Badr",
+            "lastname" =>  "Doe",
+            "email" => "sb-awlwy20334876@personal.example.com",
+            "country_code" => "US",
+            "amount" => 5,
+            "currency_code" => "USD"
+        ]);
+        Donation::create([
+            "firstname" => "Mouad",
+            "lastname" =>  "Doe",
+            "email" => "sb-9d7e120334921@personal.example.com",
+            "country_code" => "US",
+            "amount" => 5,
+            "currency_code" => "USD"
+        ]);
+        Donation::create([
+            "firstname" => "Skram",
+            "lastname" =>  "Doe",
+            "email" => "sb-xbhsm20381934@personal.example.com",
+            "country_code" => "US",
+            "amount" => 1,
+            "currency_code" => "USD"
+        ]);
     }
 }
